96990969ea2c4580dffaf659089bcc93401a2af3
WenDev <wendev1024@icloud.com>
2020-12-30 15:53:17 +0800

    [新功能]：完成了格式化输出与输入

--- cmd/server.go
+++ cmd/server.go
@@ -12,17 +12,37 @@ import (
 // 建立服务端监听，循环接入客户端，在每一个单独的协程中为每一个具体的客户端提供服务
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	sql, _ := reader.ReadString('\n')
-	sql = strings.Replace(sql, "\n", "", -1)
-	parsedSql, err := parser.Parse(sql)
-	if err != nil {
-		panic(err)
-	} else {
-		result, rows, err := parser.Handle(parsedSql)
+	fmt.Println("HSDB: A Simple DBMS")
+	fmt.Println("====================")
+
+	for {
+		fmt.Printf("->")
+		sql, _ := reader.ReadString('\n')
+		sql = strings.Replace(sql, "\n", "", -1)
+		if sql == "" {
+			continue
+		}
+		parsedSql, err := parser.Parse(sql)
 		if err != nil {
 			fmt.Println(err)
+		} else {
+			result, rows, err := parser.Handle(parsedSql)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if parsedSql.Type == parser.Select {
+				fmt.Println("Result: ")
+				for _, record := range result {
+					fmt.Printf("%-10s|", record.Field.Name)
+					for _, data := range record.Data {
+						fmt.Printf("%-10s\t|", data)
+					}
+					fmt.Println()
+				}
+				fmt.Printf("\n")
+			} else {
+				fmt.Printf("OK, %d rows changed\n", rows)
+			}
 		}
-		fmt.Println(result)
-		fmt.Println(rows)
 	}
 }
